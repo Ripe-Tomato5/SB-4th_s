e7c1418d35a39d2983de8c7952f900b39472bfa0
Ripe-Tomato5 <139101636+Ripe-Tomato5@users.noreply.github.com>
2025-09-03 17:21:00 +0900

    editted

---
+++ LoRa_test/import_serial.py
@@ -0,0 +1,37 @@
+import serial
+import sys
+import time
+import select
+
+SERIAL_LORA = "/dev/ttyS0"
+BAUDRATE = 115200
+
+def main():
+    ser_lora = serial.Serial(SERIAL_LORA, BAUDRATE, timeout=1)
+    time.sleep(5)  # Arduino縺ｮ delay(5000) 逶ｸ蠖・
+    print("LoRa bridge started. Type something and press Enter to send.")
+
+    try:
+        while True:
+            # 繧ｿ繝ｼ繝溘リ繝ｫ蜈･蜉帙′縺ゅｌ縺ｰLoRa縺ｫ騾∽ｿ｡
+            rlist, _, _ = select.select([sys.stdin], [], [], 0.01)
+            if rlist:
+                line = sys.stdin.readline()
+                line = str(line).rstrip("\n") + "\r\n"
+                print("Sending:",repr(line))
+                ser_lora.write(line.encode("ascii"))
+
+            # LoRa縺九ｉ蜿嶺ｿ｡縺後≠繧後・繧ｿ繝ｼ繝溘リ繝ｫ縺ｫ蜃ｺ蜉・           
+            if ser_lora.in_waiting > 0:
+                data = ser_lora.read(ser_lora.in_waiting)
+                sys.stdout.write(data.decode(errors="ignore"))
+                sys.stdout.flush()
+
+    except KeyboardInterrupt:
+        print("\nStopped by user")
+    finally:
+        ser_lora.close()
+
+
+if __name__ == "__main__":
+    main()
